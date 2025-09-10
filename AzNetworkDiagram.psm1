0d7b6cb732d3b36d7e29b4c77a8b5f5ae1d18226
Dan Madsen <dan@danmadsen.dk>
2025-09-10 20:32:48 +0200

    ER validation to avoid nonsense in output

--- AzNetworkDiagram.psm1
+++ AzNetworkDiagram.psm1
@@ -3086,6 +3086,22 @@ function Export-Connection {
     # ER (Peer set = ER Circuit - circuit defined seperately)
     if ($ER -and $vpngwid -ne 0) {
         $peerid = $connection.Peer.id.replace("-", "").replace("/", "").replace(".", "").ToLower()
+        $peerSub = $connection.Peer.id.Split("/")[2]
+        $peerRG = $connection.Peer.id.("/")[4]
+        $peerName = $connection.Peer.id.("/")[8]
+        
+        # Validate ER Circuit access
+        $currentcontext = (Get-AzContext).Subscription.Id
+        $tempcontext = $peerSub
+        $null = Set-AzContext $tempcontext
+        $circiut = Get-azexpressRouteCircuit -ResourceName $peerName -ResourceGroupName $peerRG -ErrorAction SilentlyContinue
+        $null = Set-AzContext $currentcontext
+        if ( $null -eq $circiut ) {
+            #Define unknown ER Cicuit here
+            $Script:Legend += ,@("Express Route Circuit","ercircuit.png")
+            $ImagePath = Join-Path $OutputPath "icons" "ercircuit.png"
+            $data += "$erportid [label = `"\nName:$peerName\n(Unknown Express route circuit)`" ; image = `"$ImagePath`";imagepos = `"tc`";labelloc = `"b`";height = 1.5;];"
+        }
         $data += "`n    $vpngwid -> $peerid`n"
     }
     # VPN or VNet2VNet
